5291bad74e67364d8aec27c81f6db92fcc5c084d
LiamOsler <liam.osler@dal.ca>
2021-11-07 22:20:27 -0400

    adjusted stylings for high resolution displays

--- website/includes/components/landing-carousel.php
+++ website/includes/components/landing-carousel.php
@@ -22,7 +22,7 @@
     </div>
 
     <div id = "layer3">
-        <div class = "container">
+        <div class = "container landing-content">
             <div class = "row">
                 <div class = "col-lg-12">
                     <br>
@@ -46,46 +46,37 @@
             </div>
 
 
-                <br>
-                <p><b>food<i class="bi bi-egg-fried egg-icon"></i>feed</b> makes it easy for you to find the best prices for take-out and delivery in your local area.</p>
-                <p>
-                <hr>
-                <div class="row">
-                    <div class="col-sm-6">
-                        <div class="card">
+            <br>
+            <p><b>food<i class="bi bi-egg-fried egg-icon"></i>feed</b> makes it easy for you to find the best prices for take-out and delivery in your local area.</p>
+            <p>
+            <hr>
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="card">
                         <div class="card-body">
-                            <h5 class="card-title">How does food feed work?</h5>
-                            <p>
-                            <i class="bi bi-cloud-arrow-up"></i><i class="bi bi-cloud-arrow-down"></i>
-                            </p>
-                            <p class="card-text">food feed checks the prices of menu items available on takeout services like UberEats, </p>
+                            <div class = "infographics">
+                                <i class="bi bi-cup-straw"></i>
+                            </div>
+                            <p class="card-text"><b>food<i class="bi bi-egg-fried egg-icon"></i>feed</b> checks the prices of menu items available on takeout services like UberEats, DoorDash, Postmates, FoodHWY and finds the best prices based on the menu item pricing, delivery charges and incidental fees.</p>
 
                             <a href="#" class="btn btn-success">Find out more</a>
                         </div>
-                        </div>
                     </div>
-                    <div class="col-sm-6">
-                        <div class="card">
+                </div>
+                <div class="col-sm-6">
+                    <div class="card">
                         <div class="card-body">
-                            <h5 class="card-title">Local Map</h5>
-                            <div id="map"></div>
-                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                            <a href="#" class="btn btn-success">Go somewhere</a>
-                        </div>
+                            <div class = "infographics">
+                                <i class="bi bi-basket3"></i>
+                            </div>
+                            <p class="card-text"><b>food<i class="bi bi-egg-fried egg-icon"></i>feed</b> checks the prices of menu items available on takeout services like UberEats, DoorDash, Postmates, FoodHWY and finds the best prices based on the menu item pricing, delivery charges and incidental fees.</p>
+
+                            <a href="#" class="btn btn-success">Find out more</a>
                         </div>
                     </div>
                 </div>
-
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tortor nulla, egestas at pulvinar vel, fermentum id mauris. Phasellus imperdiet vestibulum vulputate. Maecenas dapibus nec libero id consequat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque eu libero sodales eros mollis dictum. Maecenas ultricies nulla ut quam porttitor, nec pretium dolor venenatis. In tempus felis mi, sed finibus ipsum hendrerit quis. Cras consequat velit et magna pellentesque convallis non eget sapien.
-
-Sed vitae mollis est. Quisque non pulvinar arcu. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas placerat mauris ante, at lacinia tortor posuere ac. Mauris ac arcu diam. Morbi et lorem urna. Ut eu mattis dui, id sodales leo.
-
-Maecenas neque eros, cursus sit amet nisi vitae, ultrices fringilla neque. Sed vel lorem venenatis, vestibulum ipsum et, facilisis nisl. Aenean a tortor facilisis, rutrum mi sed, accumsan nibh. Fusce at turpis id massa tristique convallis in ut nulla. In nunc felis, laoreet sed tortor vel, malesuada posuere leo. Donec luctus odio porttitor eros maximus, id scelerisque eros mattis. Etiam consectetur dui ex, finibus auctor nulla porta nec. Donec nisl nibh, consectetur tincidunt justo et, maximus volutpat nulla. Vivamus a placerat velit. Aenean sit amet neque a lorem vestibulum fermentum. Etiam congue sapien id ullamcorper convallis. Vestibulum semper suscipit suscipit.
-
-Vivamus in imperdiet sem. Vestibulum quis accumsan metus. Integer euismod elit eu rutrum faucibus. Mauris blandit, ante auctor placerat tempus, risus felis sodales orci, vel imperdiet est felis non urna. Nulla quis dictum mi. In elementum, nibh quis vestibulum varius, leo odio interdum lorem, sed consectetur dolor metus sit amet neque. Morbi ultrices dolor at dui sodales gravida. Vivamus sagittis mi velit, non tempor nisi semper sed. Cras eleifend vehicula sapien, id facilisis felis consectetur nec.
-
-Suspendisse nisl leo, suscipit eget tellus suscipit, maximus imperdiet arcu. Praesent faucibus felis id enim feugiat, vitae pulvinar tortor dignissim. Aliquam iaculis libero sem. Cras quis tellus ipsum. Sed vel tellus purus. Sed mattis sapien a neque ullamcorper, nec mattis risus suscipit. Sed vehicula metus sit amet massa dignissim convallis. In efficitur erat massa, in tincidunt lacus scelerisque vel. Suspendisse vulputate quam id tempor congue. Donec consectetur ullamcorper justo quis feugiat.
-                </p>
+            </div>
+ </p>
 
 
         </div>
