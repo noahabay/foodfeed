2b0850656047baf267f7210fd5e41eb5b383e364
LiamOsler <liam.osler@dal.ca>
2021-11-09 01:34:39 -0400

    add team page husk and link in navigation bar

--- website/includes/components/navbar.php
+++ website/includes/components/navbar.php
@@ -6,10 +6,10 @@
   <div class="collapse navbar-collapse" id="navbarText">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">About<span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="about.php">About<span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-item nav-link" href="about.php">Team</a>
+        <a class="nav-item nav-link" href="team.php">Team</a>
       </li>
 
       <li class="nav-item">
---
+++ website/team.php
@@ -0,0 +1,20 @@
+<?php include "includes/header.php" ?>
+
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+        <h1>Who is</h1>
+        <h1 class = "brand">food<i class="bi bi-egg-fried egg-icon"></i>feed</h1>
+        <h1>?</h1>
+  </div>
+</div>
+
+<main>
+    <div class = "container">
+       
+    </div>
+</main>
+
+<script>
+</script>
+
+<?php include "includes/footer.php" ?>
