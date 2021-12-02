0bafc77aabcd05918b90b18961c42dd79bde001d
SanketShah34 <42108978+SanketShah34@users.noreply.github.com>
2021-12-02 14:17:52 -0400

    Adding newly created table blogs to the DDL file

--- database_management/foodfeed_test_database_creation.sql
+++ database_management/foodfeed_test_database_creation.sql
@@ -56,3 +56,8 @@ CREATE TABLE menuItem(
     PRIMARY KEY (itemID),
     FOREIGN KEY (menuID) REFERENCES restaurantMenu(menuID)
 	);
+
+
+DROP TABLE IF EXISTS `blogs`;
+
+CREATE TABLE `blogs` ( `blog_id` int(11) NOT NULL, `title` varchar(255) NOT NULL, `description` longtext NOT NULL, `user_id` int(11) NOT NULL, `timestamp` timestamp NOT NULL DEFAULT current_timestamp() ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;CREATE TABLE `blogs` ( `blog_id` int(11) NOT NULL, `title` varchar(255) NOT NULL, `description` longtext NOT NULL, `user_id` int(11) NOT NULL, `timestamp` timestamp NOT NULL DEFAULT current_timestamp() ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
